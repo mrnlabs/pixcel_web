c301f6285b3cf8aaaf215e0dec2fa5d6c8fb761e
Faith Ncube <56942052+mazisi@users.noreply.github.com>
2024-04-30 22:34:43 +0200

    Create Footer Component

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -7,6 +7,7 @@ import NavLink from '@/Components/NavLink.vue';
 import ResponsiveNavLink from '@/Components/ResponsiveNavLink.vue';
 import { Link } from '@inertiajs/vue3';
 import Header from '@/Components/Header.vue';
+import Footer from '@/Components/Footer.vue';
 
 const showingNavigationDropdown = ref(false);
 </script>
@@ -19,4 +20,5 @@ const showingNavigationDropdown = ref(false);
                 <slot />
             </main>
 	    </div>
+        <Footer/>
 </template>
