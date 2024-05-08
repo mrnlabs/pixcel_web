d10819f686dc8e150e1793a476eefbf1ca75f434
David Mwakajumba <mwakajumbadavid@gmail.com>
2024-05-08 16:19:41 +0200

    starting css AuthenticatedLayout

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -15,12 +15,63 @@ const showingNavigationDropdown = ref(false);
 
 <template>
     <Header/>
-    <SideNavbar/>
-    	<div class="">
+    <SideNavbar class="side"/>
+    	<div class="dark">
             <!-- hero banner here -->
-            <main>
+            <main class="main">
                 <slot />
             </main>
 	    </div>
-        <Footer/>
 </template>
+
+<style>
+.dark {
+    background-color: #E7E9EF;
+}
+.side {
+    /* margin-top: 108px; */
+    padding-top: 108px;
+    background-color: #EBEDF3;
+    /* border-radius: 10px; */
+
+    /* margin-left: 10px; */
+}
+
+.menu_left {
+    padding-top: 108px;
+}
+.menu--item {
+    /* background-color: blue; */
+    border-radius: 10px;
+    margin: 0 20px;
+}
+.menu--item:hover {
+    color: orange;
+}
+.menu--label {
+    /* background-color: blue; */
+    border-radius: 10px;
+}
+.menu--link.active {
+    background-color: #045CC7;
+    background-image: linear-gradient(to right, #045CC7,#FF6666 );
+    color: white;
+    border-radius: 10px;
+}
+.sub_menu--link:hover {
+    background-image: linear-gradient(to right, #045CC7,#FF6666 ) !important;
+	color: black !important;
+}
+
+
+.menu--item {
+	position: relative;
+	min-height: 42px;
+	line-height: 42px;
+}
+
+.menu--link.menu--label:hover {
+    background-image: linear-gradient(to right, #045CC7,#FF6666 ) !important;
+	color: white !important;
+}
+</style>
